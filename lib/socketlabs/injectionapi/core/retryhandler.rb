44a30a6b66188e923898f9e07d8f942be300a716
david.schrenker <david.schrenker@socketlabs.com>
2021-04-13 09:41:32 -0400

    adding sleep before retry

--- lib/socketlabs/injectionapi/core/retryhandler.rb
+++ lib/socketlabs/injectionapi/core/retryhandler.rb
@@ -16,7 +16,6 @@ module SocketLabs
                 attr_accessor :error_codes
 
                 def initialize(client, endpoint, settings)
-
                     @http_client = client
                     @endpoint_url = endpoint
                     @retry_settings = settings
@@ -29,43 +28,34 @@ module SocketLabs
                 def send(request)
 
                     if @retry_settings.maximum_number_of_retries == 0
-
                         response =  @http_client.send_request(request)
                         response
-
                     end
 
                     attempts = 0
 
                     loop do
-                    
                         wait_interval = @retry_settings.get_next_wait_interval(attempts)
 
                         begin
-
                             response = @http_client.send_request(request)
 
                             if (@error_codes.include? response.status_code.to_i) && (attempts < @retry_settings.maximum_number_of_retries)
-
                                 attempts += 1
-
                             else
-
                                 response
-
                             end
 
                         rescue Timeout::Error => exception
-
                             attempts += 1
                             
                             if attempts > @retry_settings.maximum_number_of_retries
                                 raise exception
                             end
+                            sleep(wait_interval)
                         
                         rescue Exception => exception
                             raise exception
-
                         end
 
                     end
