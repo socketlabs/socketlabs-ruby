188677803fc565135848e5076a0fb4af77c64d23
david-schrenker <david.schrenker@socketlabs.com>
2023-01-18 14:47:43 -0500

    changing name to key on metadata

--- lib/socketlabs/injectionapi/message/metadata.rb
+++ lib/socketlabs/injectionapi/message/metadata.rb
@@ -4,46 +4,46 @@ module SocketLabs
   module InjectionApi
     module Message
 
-      # Represents metadata as a name-value pair.
+      # Represents metadata as a key-value pair.
       # Example:
       #
       #   metadata1 = Metadata.new
-      #   metadata1.name = "name1"
+      #   metadata1.key = "key1"
       #   metadata1.value = "value1"
       #
-      #   metadata2 = Metadata.new("name2", "value2")
+      #   metadata2 = Metadata.new("key2", "value2")
 
       class Metadata
 
-        # the name of the metadata item
-        attr_accessor :name
+        # the key of the metadata item
+        attr_accessor :key
         # the value of the metadata item
         attr_accessor :value
 
         # Initializes a new instance of the Metadata class
-        # @param [String] name
+        # @param [String] key
         # @param [String] value
         def initialize(
-            name = nil,
+          key = nil,
             value = nil
         )
-          @name = name
+          @key = key
           @value = value
         end
 
         # Determines if the Metadata is valid.
         # @return [Boolean]
         def is_valid
-          valid_name = !(@name.nil? || @name.empty?)
+          valid_key = !(@key.nil? || @key.empty?)
           valid_value = !(@value.nil? || @value.empty?)
 
-          valid_name && valid_value
+          valid_key && valid_value
         end
 
-        # Represents the Metadata name-value pair as a String
+        # Represents the Metadata key-value pair as a String
         # @return [String]
         def to_s
-            "#{@name}, #{@value}"
+            "#{@key}, #{@value}"
         end
 
       end
